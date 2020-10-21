da038d0d58c815e27b2ea5859054673d5672f35a
hamaa-affix <hamaagive1108@gmail.com>
2020-10-21 14:24:30 +0900

    vuetifyDemo,vue-routerDemo

---
+++ dev-app/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        homeです
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
